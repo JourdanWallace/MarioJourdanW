18d5e49e4a62ea3a8d9f4cd83b1033f7c7296aa2
Jourdan Wallace <jaywallace713@gmail.com>
2014-12-03 09:20:23 -0800

    I got a working title screen now and I'm going to work on putting words on it tomorrow.

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -7,6 +7,8 @@ game.resources = [
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "mario", type:"image", src: "data/img/player1.png"},
+        {name: "title-screen", type:"image", src: "data/img/title-screen.png"},
+        
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -20,8 +22,7 @@ game.resources = [
         {name: "level04_1", type: "tmx", src: "data/map/level04_1.tmx"},
         {name: "level05", type: "tmx", src: "data/map/level05.tmx"}
         
-
-	/* Background music. 
+        /* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
 	 */	
--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -2,8 +2,16 @@ game.TitleScreen = me.ScreenObject.extend({
 	/**	
 	 *  action to perform on state change
 	 */
-	onResetEvent: function() {	
-		; // TODO
+    onResetEvent: function() {
+        var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
+            me.game.world.addChild(titleImage, 1);
+            me.input.bindKey(me.input.KEY.ENTER, "start");
+
+            this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
+                if (action === "start") {
+                     me.state.change(me.state.PLAY);
+                }
+            });
 	},
 	
 	
@@ -11,6 +19,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+            me.input.unbindKey(me.input.KEY.ENTER);
 	}
 });
